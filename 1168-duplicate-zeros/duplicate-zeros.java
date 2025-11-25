307ea05ccd9f9dbe2c6f74df535ccdc582b1796f
DucAnh <168915782+Tlerd@users.noreply.github.com>
2025-11-25 14:49:24 +0700

    Time: 1 ms (98.39%) | Memory: 47 MB (10.53%) - LeetSync

--- 1168-duplicate-zeros/duplicate-zeros.java
+++ 1168-duplicate-zeros/duplicate-zeros.java
@@ -1,16 +1,26 @@
 class Solution {
     public void duplicateZeros(int[] arr) {
-        Queue<Integer> queue = new LinkedList<>();
-        
-        for(int i = 0; i < arr.length; i++){
-            if (arr[i] == 0){
-                queue.add(0);
-                queue.add(0);
-            }else{
-                queue.add(arr[i]);
+        int zeros = 0;
+        for (int i : arr){
+            if (i == 0){
+                zeros++;
             }
-            Integer first = queue.poll();
-            arr[i] = first;
+        }
+        
+        int i = arr.length - 1, j = arr.length + zeros - 1;
+
+        while (i != j){
+            insert (arr, i, j--);
+            if (arr[i] == 0){
+                insert(arr, i, j--);
+            }
+            i--;
+        }
+    }
+
+    private void insert(int[] arr, int i, int j){
+        if (j < arr.length){
+            arr[j] = arr[i];
         }
     }
 }
